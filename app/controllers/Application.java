15d0efbd852a54e49d0ca3d1d3cc204e4b244ce2
akashshinde44comp <akashshinde44comp@gmail.com>
2015-04-23 19:13:59 +0530

    email config

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5062,7 +5062,7 @@ public class Application extends Controller {
 			Properties props = new Properties();
 			props.put("mail.smtp.auth", "true");
 			props.put("mail.smtp.starttls.enable", "true");
-			props.put("mail.smtp.host", "smtp.gmail.com");
+			props.put("mail.smtp.host", "smtp.office365.com");
 			props.put("mail.smtp.port", "587");
 
 			Session session = Session.getInstance(props,
